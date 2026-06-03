8b3a50e445f1fb580b2a7bf4adffac13a6beba39
didigigi <delzalval@gmail.com>
2026-06-04 01:21:41 +0200

    needs to be tested on new gmm function

--- Milestone 2/assignment7.py
+++ Milestone 2/assignment7.py
@@ -1,7 +1,8 @@
-from sheet2 import kmeans, em_gmm
+from sheet2 import kmeans, em_gmm, kmeans_agglo, agglo_dendro
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
+from scipy.cluster.hierarchy import dendrogram
 
 data = np.load('/Users/dmg/Downloads/Problem Set 2-20260520/LabML/Milestone 2/data/5_gaussians.npy').T # (2,500)
 
@@ -95,6 +96,65 @@ def plot_kmeans(X, centers, labels,path):
     plt.legend()
     plt.savefig(path)
 
+def plot_kmeans_agglo(X, R, path=None):
+    """
+    Plot hierarchical agglomerative clustering results.
+
+    Parameters
+    ----------
+    X : ndarray of shape (n_samples, 2)
+        Data points.
+    R : ndarray of shape (k-1, n_samples)
+        Cluster memberships before each agglomeration step.
+    path : str, optional
+        If given, saves the plot to this path.
+    """
+    n_steps = R.shape[0]
+
+    n_cols = 3
+    n_rows = int(np.ceil(n_steps / n_cols))
+
+    fig, axes = plt.subplots(
+        n_rows,
+        n_cols,
+        figsize=(4 * n_cols, 4 * n_rows)
+    )
+
+    axes = np.array(axes).reshape(-1)
+
+    for step in range(n_steps):
+        ax = axes[step]
+        labels = R[step]
+
+        unique_labels = np.unique(labels)
+        k = len(unique_labels)
+
+        for label in unique_labels:
+            points = X[labels == label]
+            ax.scatter(
+                points[:, 0],
+                points[:, 1],
+                s=20,
+                alpha=0.7,
+                label=f"Cluster {label}"
+            )
+
+        ax.set_title(f"{k} clusters")
+        ax.set_xlabel("x1")
+        ax.set_ylabel("x2")
+        ax.axis("equal")
+
+    for i in range(n_steps, len(axes)):
+        axes[i].axis("off")
+
+    plt.tight_layout()
+
+    if path is not None:
+        plt.savefig(path)
+
+    plt.savefig()
+
+'''
 for k in range(2,8):
     pi, mu, sigma, loglik = em_gmm(data, k)
     plot_gmm_solution(data,mu, sigma, "/Users/dmg/Downloads/Problem Set 2-20260520/LabML/Milestone 2/plots/gmm-"+str(k)+".pdf")
@@ -102,3 +162,10 @@ for k in range(2,8):
 for k in range(2,8):
     mu, labels, loss = kmeans(data, k)
     plot_kmeans(data, mu, labels, "/Users/dmg/Downloads/Problem Set 2-20260520/LabML/Milestone 2/plots/kmeans-"+str(k)+".pdf")
+'''
+for k in range(7,8):
+    centers, r, loss = kmeans(data, k)
+    R, kmloss, mergeidx = kmeans_agglo(data, r)
+    agglo_dendro(kmloss, mergeidx, "/Users/dmg/Downloads/Problem Set 2-20260520/LabML/Milestone 2/plots/agglo-"+str(k)+".pdf")
+    plot_kmeans(data, centers, r, "/Users/dmg/Downloads/Problem Set 2-20260520/LabML/Milestone 2/plots/agglo-means-"+str(k)+".pdf")
+    #plot_kmeans_agglo(data, R, "/Users/dmg/Downloads/Problem Set 2-20260520/LabML/Milestone 2/plots/agglo-"+str(k)+".pdf")
